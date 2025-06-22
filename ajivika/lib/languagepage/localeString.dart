a2d01a571f67444f0c9d404e6547f29edfaa02f8
Ashutosh-NITH <ashutosh.93343.kumar@gmail.com>
2025-06-22 08:56:43 +0530

    locale string updated

--- ajivika/lib/languagepage/localeString.dart
+++ ajivika/lib/languagepage/localeString.dart
@@ -12,11 +12,15 @@ class localeString extends  Translations{
       "preferred language" : "preferred language",
       "Continue" : "Continue",
       "Next" : "Next",
+      "Submit" : "Submit",
       "language" : "Language",
       "Tell us your profession" : "Tell us your profession",
       "Please tell us what you do and we will help you find out relevant jobs or workers" : "Please tell us what you do and we will help you find out relevant jobs or workers",
       "I am a contractor and I need workers/labours" : "I am a contractor and I need workers/labours",
       "I am a worker and I need job" : "I am a worker and I need job",
+      "Please enter": "Please enter",
+      "your phone number" : "your phone number",
+      "Phone Number" : "Phone Number",
 
     },
     'hi_IN' :{
@@ -27,11 +31,15 @@ class localeString extends  Translations{
       "preferred language" : "भाषा चुनें",
       "Continue" : "जारी रखे" ,
       "Next" : "आगे",
+      "Submit" : "दर्ज करें",
       "language" : "भाषा",
       "Tell us your profession" : "हमें अपना आजीविका बताओ",
       "Please tell us what you do and we will help you find out relevant jobs or workers" : "कृपया हमें बताएं कि आप क्या करते हैं और हम आपको प्रासंगिक नौकरी या कर्मचारी ढूंढने में मदद करेंगे",
       "I am a contractor and I need workers/labours": "मैं एक ठेकेदार हूं और मुझे श्रमिकों/मजदूरों की आवश्यकता है",
       "I am a worker and I need job" : "मैं एक मजदूर हूं और मुझे नौकरी चाहिए",
+      "Please enter": "कृपया अपना फोन नंबर ",
+      "your phone number" : "दर्ज करें",
+      "Phone Number" :  "फोन नंबर",
     }
   };
 
